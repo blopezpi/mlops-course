49c1b03989d472771cbffd0f6e0bad093877de69
Borja Lopez <borja.lopez@intelygenz.com>
2022-11-21 20:20:11 +0100

    fix: Bentoml

--- bentoml/examples/commands.sh
+++ bentoml/examples/commands.sh
@@ -5,3 +5,18 @@ bentoml yatai login --endpoint localhost:8080 --api-token xxxx
 bentoml models list
 bentoml push <bento>
 bentoml models push <model>
+
+bentoml serve-grpc service:service --enable-reflection
+grpcurl -plaintext 0.0.0.0:3000 describe
+grpcurl -d @ -plaintext 0.0.0.0:3000 bentoml.grpc.v1alpha1.BentoService/Call <<EOM
+{
+   "apiName": "classify_1",
+   "json": {
+      "sepal_length": 1.0,
+      "sepal_width": 1.0,
+      "petal_length": 1.0,
+      "petal_width": 1.0
+   }
+}
+EOM
+docker run -it --rm --network=host fullstorydev/grpcui -plaintext localhost:3000
--- bentoml/examples/iris/service.py
+++ bentoml/examples/iris/service.py
@@ -12,7 +12,7 @@ class Iris(BaseModel):
     petal_width: float
 
 
-model_1 = bentoml.models.get("iris_clf:latest")
+model_1 = bentoml.models.get("iris_clf:jcff3ndjzstme4pw")
 
 runner_1 = model_1.to_runner()
 
@@ -22,8 +22,8 @@ flowers =  ['setosa', 'versicolor', 'virginica']
 
 @service.api(input=JSON(pydantic_model=Iris),
         output=JSON())
-async def classify_1(input_data: Iris) -> Dict[str, str]:
+async def classify(input_data: Iris) -> Dict[str, str]:
     input_df = pd.DataFrame([input_data.dict()])
     result = await runner_1.predict.async_run(input_df)
-    return {"prediction": flowers[result[0]] , "model": model_1.tag}
+    return {"prediction": flowers[result[0]]}
 
